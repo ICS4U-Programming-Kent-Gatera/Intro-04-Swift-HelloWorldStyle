20116e03c538e7dc568a243e2e4fc58349620ce8
Kent Gatera <kent.gatera@stu.ocsb.ca>
2024-02-14 14:05:47 +0000

    Hello World

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,11 @@
+//
+//  HelloWorld.swift
+//
+//  Created by Kent G
+//  Created on 2022-Feb-11
+//  Version 1.0
+//  Copyright (c) Year Kent G. All rights reserved.
+//
+//  Program says Hello World.
+
+print("Hello World")
